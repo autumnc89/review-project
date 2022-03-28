b75d5884bba2a3ba1fc75820a730e3f4eb4745c3
autumnc89 <96134543+autumnc89@users.noreply.github.com>
2022-03-28 11:54:55 -0500

    First Commit

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+var password=document.getElementById("password");
+
+ function addNewPassword() {
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let pwLength = 12;
+    let password = "";
+ for (let i = 0; i <= pwLength; i++) {
+   let randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+      document.getElementById("password").value = password;
+ }
+
 // Start your code ***HERE*** =========
 
 // create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
 
 // create a global variable called "pwLength" with a number between 10 and 18
 
-// Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
+// Using the above array and password length variable, create a 
+//random password using a for loop inside of a function called "addNewPassword" either 
+//saved as an arrow function variable or a traditional function
 
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
 
